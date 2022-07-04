e47229db274c1710b8714bbe3b2a7bab4417be86
Onkar Shete <sheteonkar11@gmail.com>
2022-07-04 11:45:27 +0530

    Added java demo application files

---
+++ src/main/java/com/app/DockerHub/TestMain.java
@@ -0,0 +1,11 @@
+package com.app.DockerHub;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+public class TestMain {
+@Test
+public void testInputIsEven(){
+assertTrue(main.CheckIfInputIsAnEvenNumber(23)); //Assertion
+}
+}
---
+++ src/main/java/com/app/DockerHub/main.java
@@ -0,0 +1,16 @@
+package com.app.DockerHub;
+
+
+public class main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( CheckIfInputIsAnEvenNumber(122));
+    }
+    
+    public static boolean CheckIfInputIsAnEvenNumber(int number)
+    {
+    	return number % 2 == 0;
+    }
+    }
+
